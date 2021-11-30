ea925022374c6d08a9ca50c596115296cf65c7a8
Christophe Ferru <christophe.ferru@gmail.com>
2021-11-30 18:46:45 -0500

    test empecher le démarrage d'une autre cuissson lorsque le four est lancé

--- FourABoisESP/data/assets/script/app.js
+++ FourABoisESP/data/assets/script/app.js
@@ -2,6 +2,7 @@ const URL_API = "http://172.16.210.211/";
 var tempsActuel = 0;
 var tempsMax = 0;
 var dureeMax = 0;
+var etatFourActuel = 0;
 
 const verifConnexion = async () => {
     console.log(localStorage.getItem("token"));
@@ -29,9 +30,9 @@ const verifConnexion = async () => {
     }
 }
 
-const definirTempsActuel = (temps) => {
+/*const definirTempsActuel = (temps) => {
     document.getElementById("tempsActuel").innerHTML = temps;
-}
+}*/
 
 window.onload = async () => {
     verifConnexion();
@@ -54,43 +55,42 @@ window.onload = async () => {
 document.getElementById('typeBoisSelect').addEventListener('change', async () => {
     verifConnexion();
     let idBois = document.getElementById('typeBoisSelect').value;
-    tempsActuel = 0;
-    definirTempsActuel(tempsActuel);
+    //tempsActuel = 0;
+    //definirTempsActuel(tempsActuel);
+    if(etatFourActuel === 0){
+        if(idBois != 0){
+            const response = await fetch(URL_API+'obtenirBois?token='+localStorage.getItem("token")+'&idBois='+idBois, { method: "GET", headers: {'Accept': 'application/json', 'Content-Type': 'application/json'} });
+            const myJsonOB = await response.json();
 
-    if(idBois != 0){
-        const response = await fetch(URL_API+'obtenirBois?token='+localStorage.getItem("token")+'&idBois='+idBois, { method: "GET", headers: {'Accept': 'application/json', 'Content-Type': 'application/json'} });
-        const myJsonOB = await response.json();
+            // si resultat, on affiche les donnees
+            if(myJsonOB.code === 200){
+                // On met en place les donneees
+                document.getElementById('nomBoisDet').innerHTML = myJsonOB.donnees.nom;
+                document.getElementById('typeBoisDet').innerHTML = myJsonOB.donnees.type;
+                document.getElementById('origineBoisDet').innerHTML = myJsonOB.donnees.origine;
+                document.getElementById('tempsSechBoisDet').innerHTML = myJsonOB.donnees.sechage;
+                document.getElementById('temperatureBoisDet').innerHTML = myJsonOB.donnees.temperature;
+                dureeMax = myJsonOB.donnees.sechage;
 
-        // si resultat, on affiche les donnees
-        if(myJsonOB.code === 200){
-            // On met en place les donneees
-            document.getElementById('nomBoisDet').innerHTML = myJsonOB.donnees.nom;
-            document.getElementById('typeBoisDet').innerHTML = myJsonOB.donnees.type;
-            document.getElementById('origineBoisDet').innerHTML = myJsonOB.donnees.origine;
-            document.getElementById('tempsSechBoisDet').innerHTML = myJsonOB.donnees.sechage;
-            document.getElementById('temperatureBoisDet').innerHTML = myJsonOB.donnees.temperature;
-            dureeMax = myJsonOB.donnees.sechage;
-
-            document.getElementById('tempsTotal').innerHTML = myJsonOB.donnees.sechage;
-            document.getElementById('typeBoisChauff').innerHTML = myJsonOB.donnees.nom;
-            document.getElementById('temperatureMini').innerHTML = "min "+myJsonOB.donnees.temperature+" Celcius";
-            document.getElementById('tempVal').innerHTML = 0
-            document.getElementById('tempsActuel').innerHTML = 0;
+                document.getElementById('tempsTotal').innerHTML = myJsonOB.donnees.sechage;
+                document.getElementById('typeBoisChauff').innerHTML = myJsonOB.donnees.nom;
+                document.getElementById('temperatureMini').innerHTML = "min "+myJsonOB.donnees.temperature+" Celcius";
+                document.getElementById('tempVal').innerHTML = 0
+                //document.getElementById('tempsActuel').innerHTML = 0;
+            }
+        }else{
+            document.getElementById('nomBoisDet').innerHTML = document.getElementById('typeBoisDet').innerHTML = document.getElementById('origineBoisDet').innerHTML = document.getElementById('tempsSechBoisDet').innerHTML = document.getElementById('temperatureBoisDet').innerHTML = document.getElementById('tempsTotal').innerHTML = document.getElementById('typeBoisChauff').innerHTML = document.getElementById('temperatureMini').innerHTML = "";
         }
-    }else{
-        document.getElementById('nomBoisDet').innerHTML = document.getElementById('typeBoisDet').innerHTML = document.getElementById('origineBoisDet').innerHTML = document.getElementById('tempsSechBoisDet').innerHTML = document.getElementById('temperatureBoisDet').innerHTML = document.getElementById('tempsTotal').innerHTML = document.getElementById('typeBoisChauff').innerHTML = document.getElementById('temperatureMini').innerHTML = "";
     }
 });
 
 setInterval(async () => {
-    const response = await fetch(URL_API+'obtenirInfosFour', { method: "GET", headers: {'Accept': 'application/json', 'Content-Type': 'application/json'} });
+    const obtenirInfosFour = await fetch(URL_API+'obtenirInfosFour', { method: "GET", headers: {'Accept': 'application/json', 'Content-Type': 'application/json'} });
 
-    const myJsonSec = await response.json();
-    // si resultat, on affiche les donnees
-    console.log(myJsonSec);
+    const infosFour = await obtenirInfosFour.json();
 
-    if(myJsonSec.code === 200){
-        if(myJsonSec.donnees.dureeActuelle === dureeMax) {
+    if(infosFour.code === 200){
+        if(infosFour.donnees.dureeActuelle === dureeMax) {
             document.getElementById('demarrerFour').innerText = "Demarrer le four";
             document.getElementById('demarrerFour').classList.remove("fourActif");
 
@@ -98,8 +98,10 @@ setInterval(async () => {
             document.getElementById('delJaune').classList.remove("active");
             document.getElementById('delVerte').classList.add("active");
         }else{
-            if(myJsonSec.donnees.etatFour === 1){
-                if(myJsonSec.donnees.temperatureActuelle < myJsonSec.donnees.tempMiniBois){
+            if(infosFour.donnees.etatFour === 1){
+                etatFourActuel = 1;
+
+                if(infosFour.donnees.temperatureActuelle < infosFour.donnees.tempMiniBois){
                     document.getElementById('delRouge').classList.add("active");
                     document.getElementById('delJaune').classList.remove("active");
                     document.getElementById('delVerte').classList.remove("active");
@@ -109,41 +111,46 @@ setInterval(async () => {
                     document.getElementById('delVerte').classList.remove("active");
                 }
             }else{
+                etatFourActuel = 0;
+
                 document.getElementById('delRouge').classList.remove("active");
                 document.getElementById('delJaune').classList.remove("active");
                 document.getElementById('delVerte').classList.add("active");
             }
         }
 
-        document.getElementById('tempVal').innerHTML = myJsonSec.donnees.temperatureActuelle;
-        document.getElementById('tempsActuel').innerHTML = myJsonSec.donnees.dureeActuelle;
+        document.getElementById('tempVal').innerHTML = infosFour.donnees.temperatureActuelle;
+        document.getElementById('tempsActuel').innerHTML = infosFour.donnees.dureeActuelle;
     }
 }, 2000);
 
 document.getElementById('demarrerFour').addEventListener('click', async () => {
     //let idBois = document.getElementById('typeBoisSelect').value;
     
-    const response = await fetch(URL_API+'lancerFour', {
+    const lancerFour = await fetch(URL_API+'lancerFour', {
         method: "POST"
     });
-    const myJson = await response.json();
+    const etatFourRetour = await lancerFour.json();
 
-    console.log(myJson);
-    if(myJson.code){
-        if(myJson.code === 200){
+    if(etatFourRetour.code){
+        if(etatFourRetour.code === 200){
             document.getElementById('delRouge').classList.add("active");
             document.getElementById('delJaune').classList.remove("active");
             document.getElementById('delVerte').classList.remove("active");
 
             document.getElementById('demarrerFour').innerText = "Arreter le four";
             document.getElementById('demarrerFour').classList.add("fourActif");
-        }else if(myJson.code === 400){
+
+            etatFourActuel = 1;
+        }else if(etatFourRetour.code === 400){
             document.getElementById('delRouge').classList.remove("active");
             document.getElementById('delJaune').classList.remove("active");
             document.getElementById('delVerte').classList.remove("active");
 
             document.getElementById('demarrerFour').innerText = "Demarrer le four";
             document.getElementById('demarrerFour').classList.remove("fourActif");
+
+            etatFourActuel = 0;
         }
     }
 });
